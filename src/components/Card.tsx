81e25e1c7c3bce6d1a4abfdf04181d4b34dcb39d
Mohammed Hassan <mr.math2024@gmail.com>
2023-03-04 17:47:20 +0200

    using picture > source instead of img content: url

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,13 @@
+import mobile from "../images/image-product-mobile.jpg";
+import desktop from "../images/image-product-desktop.jpg";
+
 export default function Card() {
     return (
         <div className="card">
-            <img alt="" className="card__img" />
+            <picture>
+                <source media="(min-width: 768px)" srcSet={desktop} />
+                <img alt="Gabrielle Essence Eau De Parfum image" className="card__img" src={mobile} />
+            </picture>
             <div className="card__details">
                 <span className="card__category">Perfume</span>
                 <h1 className="card__title">Gabrielle Essence Eau De Parfum</h1>
