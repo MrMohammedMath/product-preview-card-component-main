f1b8b718db340a04ded0c4b22e69388d401f1f93
Mohammed Hassan <mr.math2024@gmail.com>
2023-03-04 14:15:39 +0200

    added card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,22 @@
 export default function Card() {
     return (
-        <>
-        <h1>sdasd</h1>
-        </>
+        <div className="card">
+            <img alt="" className="card__img" />
+            <div className="card__details">
+                <span className="card__category">Perfume</span>
+                <h1 className="card__title">Gabrielle Essence Eau De Parfum</h1>
+
+                <p className="card__paragraph">
+                    A floral, solar and voluptuous interpretation composed by Olivier Polge,
+                    Perfumer-Creator for the House of CHANEL.
+                </p>
+
+                <div className="card__price">$149.99 <span className="card__price__instead-of">$169.99</span></div>
+                <button className="card__btn">
+                    <img src="./images/icon-cart.svg" alt="" className="card__btn__icon" />
+                    Add to Cart
+                </button>
+            </div>
+        </div>
     );
 }
